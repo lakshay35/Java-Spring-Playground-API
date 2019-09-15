a39eaa48d57161dde3972dce4ad550984addd642
lakshay35@gmail.com <lakshay35@gmail.com>
2019-09-15 16:25:10 -0400

    Adding file uploading and file downloading example

--- src/main/java/com/example/springbootexample/SpringBootExampleApplication.java
+++ src/main/java/com/example/springbootexample/SpringBootExampleApplication.java
@@ -4,13 +4,19 @@ import javax.servlet.Filter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.filter.ShallowEtagHeaderFilter;
 
+import com.example.springbootexample.model.FileStorageProperties;
+
 @ServletComponentScan
 @EnableScheduling
+@EnableConfigurationProperties({
+	FileStorageProperties.class
+})
 @SpringBootApplication
 public class SpringBootExampleApplication {
 
